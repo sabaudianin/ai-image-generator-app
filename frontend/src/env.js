6ddb8bc9bf9a7608efc86b99728600839e1a56d2
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 <sabaudianin@gmail.com>
2026-02-17 18:00:53 +0100

    set server action generate image , delete image ,check credits,

--- frontend/src/env.js
+++ frontend/src/env.js
@@ -7,19 +7,19 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    DATABASE_URL: z.string().url(),
+    DATABASE_URL: z.url(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
     BETTER_AUTH_SECRET: z.string(),
-    BETTER_AUTH_URL: z.string(),
+    BETTER_AUTH_URL: z.url(),
     POLAR_ACCESS_TOKEN: z.string(),
     POLAR_WEBHOOK_SECRET: z.string(),
     AWS_SECRET_ACCESS_KEY: z.string(),
-    AWS_ACCES_KEY_ID: z.string(),
+    AWS_ACCESS_KEY_ID: z.string(),
     AWS_REGION: z.string(),
     AWS_S3_BUCKET_NAME: z.string(),
-    MODAL_ZIMAGE_URL: z.string().url(),
+    MODAL_ZIMAGE_URL: z.url(),
   },
 
   /**
@@ -43,7 +43,7 @@ export const env = createEnv({
     POLAR_ACCESS_TOKEN: process.env.POLAR_ACCESS_TOKEN,
     POLAR_WEBHOOK_SECRET: process.env.POLAR_WEBHOOK_SECRET,
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
-    AWS_ACCES_KEY_ID: process.env.AWS_ACCES_KEY_ID,
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
     AWS_REGION: process.env.AWS_REGION,
     AWS_S3_BUCKET_NAME: process.env.AWS_S3_BUCKET_NAME,
     MODAL_ZIMAGE_URL: process.env.MODAL_ZIMAGE_URL,
